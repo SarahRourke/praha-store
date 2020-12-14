a6f79e75c10004f032edefad179a03b8b317c4a3
sarahrourke <serourke@gmail.com>
2020-12-14 11:26:49 -0600

    logo and brandstatement styled

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,29 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Items from './components/ItemList';
-import Item from '/components/Item';
-import Home from './components/Home';
-import EditItemForm from './components/EditItemForm';
-import AddItemForm from './components/AddItemForm';
+// import Header from './components/Header';
+// import Items from './components/ItemList';
+// import Item from '/components/Item';
+// import Home from './components/Home';
+// import EditItemForm from './components/EditItemForm';
+// import AddItemForm from './components/AddItemForm';
 
 
 function App(props) {
   return (
     <div className="App">
-      <Header />
-        <aside className="mainlogo">praha</aside>
-          <div className="main-container">
-            <Switch>
+      {/* <Header /> */}
+        <aside className="Main-logo">p r a h a</aside>
+        <br />
+        <aside className="Brand-statement">european cottage for contemporary lifestyles</aside>
+          <div className="Main-container">
+            {/* <Switch>
               <Route exact path="/" component={Home}/>
               <Route exact path="/items" component={Items}/>
               <Route exact path="/items/:id" component={Item}/>
               <Route exact path="/create" component={AddItemForm}/>
               <Route exact path="/items/:id/update" component={EditItemForm}/>
-            </Switch>
+            </Switch> */}
           </div>
     </div>
   );
