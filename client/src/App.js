d00e5fe0b470f406b895a3bd0573faf616e6ea87
sarahrourke <serourke@gmail.com>
2020-12-14 10:17:06 -0600

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Items from './components/ItemList';
+import Item from '/components/Item';
+import Home from './components/Home';
+import EditItemForm from './components/EditItemForm';
+import AddItemForm from './components/AddItemForm';
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+      <Header />
+        <aside className="mainlogo">praha</aside>
+          <div className="main-container">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/items" component={Items}/>
+              <Route exact path="/items/:id" component={Item}/>
+              <Route exact path="/create" component={AddItemForm}/>
+              <Route exact path="/items/:id/update" component={EditItemForm}/>
+            </Switch>
+          </div>
     </div>
   );
 }
