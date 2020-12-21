ee2aaa42ec00e85f2c71e276e31532e3101ac158
sarahrourke <serourke@gmail.com>
2020-12-21 15:50:59 -0600

    working on udating item.image

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import Items from './components/Items';
 import Item from './components/Item';
 import Home from './components/Home';
-// import EditItemForm from './components/EditItemForm';
+import EditItemForm from './components/EditItemForm';
 import AddItemForm from './components/AddItemForm';
 
 
@@ -22,7 +22,7 @@ function App(props) {
               <Route exact path="/items" component={Items}/>
               <Route exact path="/items/:id" component={Item}/>
               <Route exact path="/create" component={AddItemForm}/>
-              {/* <Route exact path="/items/:id/update" component={EditItemForm}/> */}
+              <Route exact path="/items/:id/update" component={EditItemForm}/>
             </Switch>
           </div>
     </div>
