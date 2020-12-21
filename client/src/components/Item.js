ba72a79a9c544417b20a87ae165f7eb8e3ff2a07
sarahrourke <serourke@gmail.com>
2020-12-21 12:50:30 -0600

    Item.js

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -21,6 +21,41 @@ const Item = (props) => {
                 console.log(resp.data);
                 setLoaded(true);
             })
-        })
+            .catch(error => console.log(error));}, [props.match.params.id]);
+
+        const deleteItem = (id) => {
+            axios.delete(`/api/v1/items/${item.id}`)
+            .then(resp => {
+                props.history.push('/items');
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        };
+
+        return (
+            <Container fluid='md' className="ItemContainer">
+                {
+                    loaded &&
+                        <>
+                            <Card>
+                                <h5>Item Details</h5>
+                                <Card.Img src={item.image_url} alt="add Image"/>
+                                <Card.Body>
+                                    <Card.Title>{item.name}</Card.Title>
+                                    <Card.Text>${item.price}</Card.Text>
+                                    <Card.Text>{item.category}</Card.Text>
+                                </Card.Body>
+                                /* delete button redirects to Itemsj page and edit button redirects to item update page */
+                                <Link to={`/items/${item.id}/update`}><Button variant="outline-info">Edit</Button></Link>
+                                <Button type="submit" variant="outline-danger" onClick={() => deleteItem(item.id)}>Delete</Button>
+                            </Card>
+                        </>
+                }
+            </Container>
+        )
+        
 }
+
+export default Item;
 
