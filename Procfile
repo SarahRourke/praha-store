ae9dcaebe5a4a2f9627207ed737f1e3ab3a03128
sarahrourke <serourke@gmail.com>
2021-01-05 15:45:45 -0600

    changed web command to bundle exec rails server -p

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec rails s $PORT
+web: bundle exec rails server -p $PORT
 release: bin/rake db:migrate
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
   
-  
+  root 'praha_hompage#index'
 
   namespace :api do 
     namespace :v1 do
 
       # calls images to "/" to give active_record attachments entryway to app w/ in namespace ::api::v1
       
-      
+      get '/', to: 'items#index', via: :all
       # calls items#index to '/api/v1/items'
       resources :items, path: '/items'
 
