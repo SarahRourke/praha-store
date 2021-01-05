350c84ee11864c00afa26cbd4cd31e62ff34580d
sarahrourke <serourke@gmail.com>
2021-01-05 04:05:28 -0600

    redis?

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       # calls images to "/" to give active_record attachments entryway to app w/ in namespace ::api::v1
-      resources :images
+      
       
       # calls items#index to '/api/v1/items'
       resources :items, path: '/items'
 
       # calls items by :id to `/api/v1/items/${id}`
       resources :items, param: :id, path: '/items/:id'
-
+     
     end
 
   end
  
-  get '*path', to: 'praha_homepage#index' 
+  get '/*path', to: 'praha_homepage#index' 
  
   # get '/*path', to: 'items#index', via: :all
   # match 'items', to: 'items#index', via: [:get, :post]
