98c38a3b08be0190c5d405591b2e68e7b832e673
sarahrourke <serourke@gmail.com>
2021-01-05 05:42:46 -0600

    fixed additem form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  root "praha_homepage#index"
+  
 
   namespace :api do 
     namespace :v1 do
 
       # calls images to "/" to give active_record attachments entryway to app w/ in namespace ::api::v1
-      
+      root "items#index"
       
       # calls items#index to '/api/v1/items'
       resources :items, path: '/items'
