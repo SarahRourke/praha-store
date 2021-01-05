b91d002577c1ca11ae012834bce6bab99a096ee8
sarahrourke <serourke@gmail.com>
2021-01-05 06:26:47 -0600

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'praha_homepage#index'
   
 
   namespace :api do 
     namespace :v1 do
 
       # calls images to "/" to give active_record attachments entryway to app w/ in namespace ::api::v1
-      root "items#index"
+      get '/', to: "items#index"
       
       # calls items#index to '/api/v1/items'
       resources :items, path: '/items'
