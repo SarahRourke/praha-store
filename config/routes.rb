72db91887256bb69cba930e59a263572c4725868
sarahrourke <serourke@gmail.com>
2021-01-05 06:41:40 -0600

    changed homepage to '/' in p.json of client

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
     namespace :v1 do
 
       # calls images to "/" to give active_record attachments entryway to app w/ in namespace ::api::v1
-      get '/', to: "items#index"
+      
       
       # calls items#index to '/api/v1/items'
       resources :items, path: '/items'
 
       # calls items by :id to `/api/v1/items/${id}`
       resources :items, param: :id, path: '/items/:id'
+
+      get '/', to: 'items#index'
      
     end
 
